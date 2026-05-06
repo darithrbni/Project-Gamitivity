91850a7f338d85c90c59c1446eb4d9b2e5f1c7fb
Darith <phoexlow@gmail.com>
2026-05-06 20:12:55 +0700

    Placeholder Corkboard Interaktif

--- Frontend/src/components/Corkboard.jsx
+++ Frontend/src/components/Corkboard.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react";
+
+import idleImage from "../assets/placeholderIdle.png";
+import hoverImage from "../assets/placeholderHover.png";
+import clickImage from "../assets/placeholderClick.png";
+
 function Corkboard() {
+  const [boardState, setBoardState] = useState("idle");
+
+  function getCurrentImage() {
+    if (boardState === "hover") return hoverImage;
+    if (boardState === "click") return clickImage;
+
+    return idleImage;
+  }
+
   return (
-    <div className="corkboard"></div>
+    <img
+      className="corkboard"
+      src={getCurrentImage()}
+      alt="Corkboard"
+      onMouseEnter={() => setBoardState("hover")}
+      onMouseLeave={() => setBoardState("idle")}
+      onMouseDown={() => setBoardState("click")}
+      onMouseUp={() => setBoardState("hover")}
+    />
   );
 }
 
